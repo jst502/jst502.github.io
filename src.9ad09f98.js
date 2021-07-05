286ebcc6b81f2b3023f17b0bf07a223c679bd113
sunjunjie <sunjunjie132@126.com>
2021-07-06 03:07:48 +0800

    1364cfb892f82fac2dc64f5db49a932cd77a853d:2021-07-06 03:07:48

--- src.9ad09f98.js
+++ src.9ad09f98.js
@@ -218,7 +218,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61539" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60836" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
